a7929e1815030a6fb2cac3805181d39af4cf6ff4
sarthwa8 <sarthakvs10@gmail.com>
2025-04-20 22:10:40 +0530

    Second commit

---
+++ src/main.py
@@ -0,0 +1,73 @@
+import os
+import json
+
+import streamlit as st
+from openai import OpenAI
+
+
+#configuring openai-api key
+working_dir = os.path.dirname(os.path.realpath(__file__))
+config_data = json.load(open(os.path.join(working_dir, "config.json")))
+OPENAI_API_KEY = config_data["OPENAI_API_KEY"]
+
+#initialise OpenAI client using new SDK
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+#configuring streamlit page
+st.set_page_config(
+    page_title="GPT-4o Chat",
+    page_icon ="🗣️",
+    layout = "centered"
+)
+
+
+#initialise chat session in streamlit if not present
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+#streamlit page title
+st.title('📜GPT-4o ChatBot')
+
+#display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+#input field for user's message
+user_prompt = st.chat_input("Ask this bot something...")
+
+if user_prompt:
+    # add user's message to chat and display
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content":user_prompt})
+
+    #send user's message to gpt.4o and get a response
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                *st.session_state.chat_history
+                ]
+        )
+
+        assistant_response = response.choices[0].message.content
+
+
+         # display gpt-4o's response
+        with st.chat_message("assistant"):
+            st.markdown(assistant_response)
+
+        st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    except Exception as e:
+        st.error(f"OpenAI API Error: {e}")
+
+
+
+
+
+
+
+
