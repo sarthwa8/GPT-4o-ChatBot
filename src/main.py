ace430e2dd3ed8282d28173da69e3e4ec5b87d1e
sarthwa8 <sarthakvs10@gmail.com>
2025-04-20 23:12:21 +0530

    some commit

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 #configuring openai-api key
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
-print(f"Loaded API Key: {OPENAI_API_KEY}")
+print(f"Loaded API Key: {api_key}")
 
 #initialise OpenAI client using new SDK
 client = OpenAI(api_key=api_key)
